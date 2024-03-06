63442ac0f760b8c1dbf5c6468579a8804f2188c2
Ibrahim-el-moudden <79118459+Ibrahim-el-moudden@users.noreply.github.com>
2024-03-06 14:27:04 +0100

    margin gefixt

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -5,7 +5,7 @@ export function MyCard(props){
 
     return(
         <Col className="text-center" >
-            <Card className={`m-5 p-2 shadow-sm`} style={{ width: '20rem' }}>
+            <Card className={`m-2 p-2 shadow-sm`} style={{ width: '18.5rem' }}>
                     {children}
             </Card>
         </Col>
